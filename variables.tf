781eaa76368e8ce128e6e03b06ddc9c85eb98f36
kmei0428 <mei.kojima.428@gmail.com>
2025-04-14 15:25:32 +0900

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "pet_length" {
+    description = "Length of the pet name"
+    type        = number
+    default     = 4
+}
+
+variable "pet_prefix" {
+    description = "Prefix of the pet name"
+    type        = string
+    default     = "bootcamp"
+}
