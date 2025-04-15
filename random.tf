8db007e7396d9b5b77b55cd75956d037ffc77c50
kmei0428 <mei.kojima.428@gmail.com>
2025-04-15 11:36:35 +0900

    Update random.tf

--- random.tf
+++ random.tf
@@ -7,3 +7,13 @@ resource "random_pet" "example_2" {
     length = var.pet_length
     prefix = var.pet_prefix
 }
+
+resource "random_pet" "example_3" {
+    length = var.pet_length
+    prefix = var.pet_prefix
+}
+
+resource "random_pet" "example_4" {
+    length = var.pet_length
+    prefix = var.pet_prefix
+}
